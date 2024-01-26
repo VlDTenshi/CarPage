3149671a0e59187add625b7584da42c6f8298a48
VlDTenshi <118188335+VlDTenshi@users.noreply.github.com>
2024-01-26 20:19:38 +0200

    Добавьте файлы проекта.

---
+++ CarPage/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CarPage.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
